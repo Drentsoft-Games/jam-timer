18bfa522c2fad5967229eb58628532ccf9f384c0
drentsoft <1520613+drentsoft@users.noreply.github.com>
2022-08-12 21:03:22 +0100

    Initial commit.

---
+++ JamTimer.gd
@@ -0,0 +1,126 @@
+# Jam Timer
+# Copyright 2022 (c) Drentsoft Games Ltd
+
+extends Control
+
+signal timeout
+
+var time_left: float
+var modified_time: float = 0.0
+
+var running: bool = false
+
+var alarm_triggered: bool = false
+var show_controls: bool = true
+
+onready var timer_label = $HBoxContainer/TimerLabel
+onready var controls = $HBoxContainer/Controls
+onready var mute = $HBoxContainer/Controls/Mute
+onready var alarm = $TimeoutAlarm
+
+func _ready():
+	connect("timeout", self, "on_timeout")
+	mute.visible = false
+	timer_label.text = get_time_left_text(time_left)
+
+func set_time_left(time: float):
+	time_left = time
+	timer_label.text = get_time_left_text(time_left)
+	print_debug("Time left set to %s" % time)
+
+func get_time_left_text(time, show_millis: bool = false) -> String:
+	var neg: bool = false
+	if time < 0.0:
+		neg = true
+		time = abs(time)
+	var time_str = "%02d:%02d:%02d"
+	var sec = int(time) % 60
+	var mins = int(time / 60.0) % 60
+	var hours = int(time / 3600.0) % 24
+	var days = (time / 86400.0)
+	var millis = fmod(time, 1.0)
+	var values = [hours, mins, sec]
+	if show_millis:
+		values.append(millis)
+		time_str += ".%04d"
+	if days > 1.0:
+		time_str = "%02d:" + time_str
+		values.insert(0, days)
+	if neg:
+		time_str = "-" + time_str
+	print(time_str)
+	print(values)
+	return time_str % values
+
+func _process(delta):
+	if running:
+		time_left -= delta
+		timer_label.text = get_time_left_text(time_left)
+		if modified_time > 0.0:
+			modified_time -= delta
+			if modified_time < 0.0:
+				modified_time = 0.0 # Display modified time on the UI
+		if time_left <= 0.0:
+			if !alarm_triggered:
+				emit_signal("timeout")
+
+func _on_Controls_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_RIGHT and event.pressed:
+			show_controls = !show_controls
+			for child in controls.get_children():
+				child.visible = show_controls
+			mute.visible = (alarm_triggered and show_controls)
+
+func _on_StartPause_toggled(button_pressed):
+	running = button_pressed
+	if alarm_triggered:
+		if !running:
+			alarm.stop()
+		else:
+			alarm.play() # Restart after pause
+
+func _on_Mute_pressed():
+	pass # Replace with function body.
+
+func _on_Mute_toggled(button_pressed):
+	print("Muting?")
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Alarm"), button_pressed)
+
+# Returns to setup
+func _on_Stop_pressed():
+	running = false
+	alarm.stop()
+	hide()
+
+func on_timeout():
+	alarm_triggered = true
+	mute.visible = true
+	$TimeoutAlarm.play()
+
+func _on_TimeoutAlarm_finished():
+	if alarm_triggered and running:
+		alarm.play() # Just keep repeating
+
+func check_mod_keys():
+	var amt = 1.0
+	if Input.is_key_pressed(KEY_SHIFT):
+		amt = 10.0
+	if Input.is_key_pressed(KEY_CONTROL):
+		amt *= 5.0
+	return amt
+
+func _on_MinusTime_pressed():
+	var amt = check_mod_keys()
+	time_left -= amt
+	if time_left < 0.0 and running and !alarm_triggered:
+		emit_signal("timeout")
+	modified_time -= amt
+
+func _on_PlusTime_pressed():
+	var amt = check_mod_keys()
+	time_left += amt
+	if time_left > 0.0 and alarm_triggered:
+		alarm_triggered = false
+		alarm.stop()
+	modified_time += amt
---
+++ Setup.gd
@@ -0,0 +1,81 @@
+# Jam Timer
+# Copyright 2022 (c) Drentsoft Games Ltd
+
+extends Control
+
+var alarms = {}
+
+var show_millis: bool = false
+
+onready var bg_panel = $BGPanel
+onready var settings = $Settings
+
+onready var hours = $Settings/HBoxContainer/Hours
+onready var mins = $Settings/HBoxContainer/Mins
+onready var sec = $Settings/HBoxContainer/Sec
+
+onready var timer_label = $Settings/TimerLabel
+
+onready var alarm_drop = $Settings/Alarm
+onready var alarm_preview = $AlarmPreview
+onready var trans_bg = $Settings/TransparentBG
+onready var timer = $JamTimer
+
+func _ready():
+	get_tree().get_root().set_transparent_background(trans_bg.pressed)
+	timer.hide()
+	load_alarms()
+	_on_3Hours_pressed()
+
+func load_alarms():
+	var dir = Directory.new()
+	var err = dir.open("res://audio/alarms/")
+	assert(err == OK)
+	dir.list_dir_begin(true, true)
+	var f = dir.get_next()
+	while f != "":
+		if f.ends_with(".import"):
+			f = dir.get_next()
+			continue
+		var alarm = load("%s/%s" % [dir.get_current_dir(), f])
+		alarms[f] = alarm
+		f = dir.get_next()
+	dir.list_dir_end()
+	alarm_drop.add_item("No Alarm")
+	for alarm in alarms.keys():
+		alarm_drop.add_item(alarm)
+
+func set_preview_audio(id):
+	print_debug("Want to preview audio %s" %id)
+	if id > -1:
+		alarm_preview.stream = alarms.get(alarms.keys()[id])
+	else:
+		alarm_preview.stream = null
+
+func _on_3Hours_pressed():
+	timer.set_time_left(10800.0) # 3 hours in seconds
+	timer_label.text = timer.get_time_left_text(timer.time_left)
+
+func _on_SetCustom_pressed():
+	timer.set_time_left((hours.value * 3600.0) + (mins.value * 60.0) + sec.value)
+	timer_label.text = timer.get_time_left_text(timer.time_left)
+
+func _on_Alarm_item_selected(index):
+	set_preview_audio(index - 1)
+
+func _on_Preview_pressed():
+	alarm_preview.play()
+
+func _on_TransparentBG_toggled(button_pressed):
+	get_tree().get_root().set_transparent_background(button_pressed)
+
+func _on_ShowTimer_pressed():
+	settings.hide()
+	bg_panel.hide()
+	timer.show()
+	if alarm_drop.get_selected_id() > 0:
+		timer.alarm.stream = alarms.get(alarms.keys()[alarm_drop.get_selected_id() - 1])
+
+func _on_Stop_pressed():
+	settings.show()
+	bg_panel.show()
